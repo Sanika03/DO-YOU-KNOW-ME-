fb20a475b07c1d9f157ad382de860927ddfbf3fa
Sanika Suryawanshi <84778109+Sanika03@users.noreply.github.com>
2022-08-25 11:27:09 +0000

    added feature for user input

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
 var readlineSync = require('readline-sync');
-
-var questions = [{
-    question : 'What is my favourite colour ? ',
-    answer : 'Black'
-  },{
-    question : 'What would I choose : Going to a Wedding or to a Club ? ',
-    answer : 'Club'
-  },{
-    question : 'Did I ever have braces : Yes or No ? ',
-    answer : 'Yes'
-  },{
-    question : 'What pet do I have ? ',
-    answer : 'Dog'
-  },{
-    question : 'What is my height in feet ? ',
-    answer : '5.6'
-  }];
